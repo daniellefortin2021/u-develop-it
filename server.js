b9e10dc4e12ed1636b1696d75e9f890d845cb288
Danielle Fortin <fortind6@gmail.com>
2022-02-06 19:31:46 -0500

    added additional db queries

--- server.js
+++ server.js
@@ -30,8 +30,36 @@ app.get('/', (req,res) => {
 });
 
 //return all data in the candidates tables
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+//return a single candidate from the candidates table
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+//delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 //route to handle user requests that aren't supported by the app - should be last GET route
