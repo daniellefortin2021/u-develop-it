66ffe790ac70638ffcb0ca0a7994ea7f5b7b3bc2
Danielle Fortin <fortind6@gmail.com>
2022-02-09 23:18:21 -0500

    set up voter routes

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
-
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//adding express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//use apiRoutes
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-//route to handle user requests that aren't supported by the app - should be last GET route
-app.use((req,res) => {
-    res.status(404).end();
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
 });
 
-//adds server to PORT
-app.listen(PORT, () =>{
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+  });
 });
